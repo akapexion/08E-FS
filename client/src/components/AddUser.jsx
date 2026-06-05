0eba40cb0c7344ac8552b168f8e3845c5211884f
Irzan Ahmad <irzanahmad44@gmail.com>
2026-06-05 16:53:40 +0500

    first commit

--- client/src/components/AddUser.jsx
+++ client/src/components/AddUser.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddUser = () => {
+
+    const [username, setUsername] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e);
+
+        console.log(username);
+
+        setUsername("");
+    }
+
     return (
         <div>
 
-            <form className='flex justify-center my-24'>
-            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-                <legend className="fieldset-legend">Settings</legend>
-                <div className="join">
-                    <input type="text" className="input join-item" placeholder="Product name" />
-                    <button className="btn join-item">save</button>
-                </div>
-            </fieldset>
+            <form onSubmit={handleSubmit} className='flex justify-center my-24'>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                    <legend className="fieldset-legend">Add User</legend>
+                    <div className="join">
+                        <input type="text" className="input join-item" placeholder="User Name Here..." value={username} onChange={(e) => setUsername(e.target.value)} />
+                        <button className="btn join-item">save</button>
+                    </div>
+                </fieldset>
             </form>
         </div>
     )
