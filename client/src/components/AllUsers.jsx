f6fc9336203d4cfd45ed3b3789573e49151157fb
Irzan Ahmad <irzanahmad44@gmail.com>
2026-06-17 17:03:13 +0500

    ---

--- client/src/components/AllUsers.jsx
+++ client/src/components/AllUsers.jsx
@@ -4,6 +4,10 @@ import React, { useEffect, useState } from 'react'
 const AllUsers = () => {
 
     const [users, setUsers] = useState([]);
+    const [editingId, setEditingId] = useState(null);
+    const [editName, setEditName] = useState("");
+
+
     var i = 1;
 
     const getUsers = async () => {
@@ -25,6 +29,29 @@ const AllUsers = () => {
         getUsers();
     }, []);
 
+    const handleEdit = (u) => {
+        setEditingId(u._id);
+        setEditName(u.user_name);
+
+        console.log(editingId);
+    }
+
+    const saveEdit = async(id) => {
+
+        const base_url = "http://localhost:3000";
+
+        try{
+            const response = await axios.put(`${base_url}/updateuser/${id}`, {
+                editName
+            });
+
+            console.log(response);
+        }
+        catch(err){
+            console.log("Error Updating Data", err);
+        }
+    }
+
 
 
 
@@ -43,14 +70,36 @@ const AllUsers = () => {
                     <tbody>
 
                         {users.map((u) => (
-                        <tr>
-                            <th>{i++}</th>
-                            <td>{u.user_name}</td>
-                            <td className='flex gap-3'>
-                                <button className="btn btn-soft btn-info btn-sm">Edit</button>
-                                <button className="btn btn-soft btn-error btn-sm">Delete</button>
-                            </td>
-                        </tr>
+                            <tr>
+                                <th>{i++}</th>
+                                <td>{
+                                
+                                editingId == u._id ?
+
+                                <input type="text" className='border border-gray-300 p-3' value={editName} onChange={(e) => setEditName(e.target.value)} />
+                                :
+                                u.user_name
+                                
+                                }</td>
+                                <td className='flex gap-3'>
+
+                                    {editingId == u._id
+                                        ?
+                                       <>
+                                            <button className="btn btn-soft btn-success btn-sm" onClick={() => handleSave(u._id)}>Save</button>
+                                            <button className="btn btn-soft btn-warning btn-sm" onClick={() => setEditingId(null)}>Cancel</button>
+                                        </> 
+                                :
+
+                                        <>
+                                            <button className="btn btn-soft btn-info btn-sm" onClick={() => handleEdit(u)}>Edit</button>
+                                            <button className="btn btn-soft btn-error btn-sm">Delete</button>
+                                        </>
+                                    }
+
+
+                                </td>
+                            </tr>
                         ))}
 
                     </tbody>
